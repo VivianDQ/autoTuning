ebbef1d596d3a566015a2c10fefb82ab6d7c05e4
VivianDQ <qding@ucdavis.edu>
2021-04-14 20:10:14 -0700

    dist

--- run_grid.py
+++ run_grid.py
@@ -4,6 +4,7 @@ import math
 import os
 import argparse
 import importlib
+from scipy.stats import truncnorm
 
 # from algorithms.data_generator import *
 from algorithms.LinUCB import *
@@ -14,13 +15,14 @@ data_generator = importlib.import_module('algorithms.data_generator')
 
 parser = argparse.ArgumentParser(description='simulations')
 parser.add_argument('-split', '--split', type=float, default = 0.5, help = 'split size of candidate exploration rates')
-parser.add_argument('-rep', '--rep', type=int, default = 5, help = 'repeat times')
+parser.add_argument('-rep', '--rep', type=int, default = 10, help = 'repeat times')
 parser.add_argument('-algo', '--algo', type=str, default = 'linucb', help = 'can also be lints, glmucb')
 parser.add_argument('-model', '--model', type=str, default = 'linear', help = 'linear or logistic')
 parser.add_argument('-k', '--k', type=int, default = 100, help = 'number of arms')
 parser.add_argument('-max_rate', '--max_rate', type=float, default = 10, help = 'max explore rate')
 parser.add_argument('-t', '--t', type=int, default = 10000, help = 'total time')
 parser.add_argument('-d', '--d', type=int, default = 5, help = 'dimension')
+parser.add_argument('-dist', '--dist', type=str, default = 'uniform', help = 'can also be normal')
 parser.add_argument('-data', '--data', type=str, default = 'grid_all', help = 'grid search')
 parser.add_argument('-lamda', '--lamda', type=float, default = 1, help = 'lambda, regularization parameter')
 parser.add_argument('-delta', '--delta', type=float, default = 0.1, help = 'error probability')
@@ -29,6 +31,7 @@ parser.add_argument('-lamdas', '--lamdas', type=list, default = [0.1,0.5,1], hel
 args = parser.parse_args()
 
 explore_interval_length = args.split
+dist = args.dist
 T = args.t
 d = args.d
 rep = args.rep
@@ -45,7 +48,9 @@ if not os.path.exists('results/'):
     os.mkdir('results/')
 if not os.path.exists('results/' + datatype + '/'):
     os.mkdir('results/' + datatype + '/')
-path = 'results/' + datatype + '/'
+if not os.path.exists('results/' + datatype + '/' + algo + '/'):
+    os.mkdir('results/' + datatype + '/' + algo + '/')
+path = 'results/' + datatype + '/' + algo + '/'
 
 ub = 1/math.sqrt(d)
 lb = -1/math.sqrt(d)
@@ -61,14 +66,19 @@ for explore in J:
     for i in range(rep):
         # print(i, ": ", end = " ")
         np.random.seed(i+1)
+        theta = np.random.uniform(lb, ub, d)
         if 'lin' in algo:
-            theta = np.random.uniform(lb, ub, d)
-            fv = np.random.uniform(lb, ub, (T, K, d))
+            if dist == 'uniform':
+                fv = np.random.uniform(lb, ub, (T, K, d))
+            elif dist == 'normal':
+                fv = truncnorm.rvs(lb, ub, size=(T,K,d))
             context = data_generator.context
             bandit = context(K, T, d, sigma, true_theta = theta, fv=fv)
         elif 'glm' in algo:
-            theta = np.random.uniform(lb, ub, d)
-            fv = np.random.uniform(-1, 1, (T, K, d))
+            if dist == 'uniform':
+                fv = np.random.uniform(lb, ub, (T, K, d))
+            elif dist == 'normal':
+                fv = truncnorm.rvs(-1, 1, size=(T,K,d))
             context_logistic = data_generator.context_logistic
             bandit = context_logistic(K, -1, 1, T, d, sigma, true_theta = theta, fv=fv)
         bandit.build_bandit()
@@ -86,4 +96,4 @@ for explore in J:
     print("explore = {} done!".format(explore))
     
 final = np.array([ [k] + v for k,v in final.items() ])       
-np.savetxt(path + algo, final)   
+np.savetxt(path + dist, final)   
